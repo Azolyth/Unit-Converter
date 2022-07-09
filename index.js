3dd1ea5052cf6c73f0c908c7377863100969e166
Silas <azolyth@gmail.com>
2022-07-08 21:30:04 -0700

    Setup unit conversion logic

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const convertInput = document.getElementById('convert-input');
+
+const convert = () => {
+    convertLength();
+    convertVolume();
+    convertMass();
+};
+// Converts meters to feet & feet to meters
+const convertLength = () => {
+    const feet = Number(convertInput.value * 3.281).toFixed(3);
+    const meters = Number(convertInput.value / 3.281).toFixed(3);
+
+    document.getElementById('length-el').innerHTML = `
+        <p>${convertInput.value} meters = ${feet} feet |
+         ${convertInput.value} feet = ${meters} meters
+        </p>
+    `
+};
+// Converts liters to gallons & gallons to liters
+const convertVolume = () => {
+    const gallons = Number(convertInput.value * 0.264).toFixed(3);
+    const liters = Number(convertInput.value / 0.264).toFixed(3);
+
+    document.getElementById('volume-el').innerHTML = `
+        <p>${convertInput.value} liters = ${gallons} gallons |
+            ${convertInput.value} gallons = ${liters} liters
+        </p>
+    `
+};
+// Kilograms to pounds
+const convertMass = () => {
+    const pounds = Number(convertInput.value * 2.204).toFixed(3);
+    const kilos = Number(convertInput.value / 2.204).toFixed(3);
+
+    document.getElementById('mass-el').innerHTML = `
+        <p>${convertInput.value} kilos = ${pounds} pounds |
+            ${convertInput.value} pounds = ${kilos} kolos
+        <p>
+    `
+};
+// Converts all units on click via convert()
+document.getElementById('convert-button').addEventListener('click', convert);
