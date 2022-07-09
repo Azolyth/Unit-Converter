d1d8a5ecc4a83ef28cd7bf3c7f28894826ba7994
Silas <azolyth@gmail.com>
2022-07-08 22:20:11 -0700

    Setup css layout

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const convertLength = () => {
     const meters = Number(convertInput.value / 3.281).toFixed(3);
 
     document.getElementById('length-el').innerHTML = `
+        <h3>Length (Meter/Feet)</h3>
         <p>${convertInput.value} meters = ${feet} feet |
          ${convertInput.value} feet = ${meters} meters
         </p>
@@ -24,6 +25,7 @@ const convertVolume = () => {
     const liters = Number(convertInput.value / 0.264).toFixed(3);
 
     document.getElementById('volume-el').innerHTML = `
+        <h3>Volume (Liters/Gallons)</h3>
         <p>${convertInput.value} liters = ${gallons} gallons |
             ${convertInput.value} gallons = ${liters} liters
         </p>
@@ -35,6 +37,7 @@ const convertMass = () => {
     const kilos = Number(convertInput.value / 2.204).toFixed(3);
 
     document.getElementById('mass-el').innerHTML = `
+        <h3>Mass (Kilograms/Pounds)</h3>
         <p>${convertInput.value} kilos = ${pounds} pounds |
             ${convertInput.value} pounds = ${kilos} kolos
         <p>
